24502318f17ed43f0bfc727b1b8e207f3b75588e
Nicholas Nikas <nicholas.nikas@outlook.com>
2020-01-13 16:30:48 -0500

    deploying

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitRegister = () => {
-    fetch('https://serene-waters-61221.herokuapp.com/register', {
+    fetch('http://localhost:3000/register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
